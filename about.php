00bb7f58fd7a09a1aaf2b64b7d725e795dfc9660
XhmikosR <xhmikosr@users.sourceforge.net>
2014-03-04 19:28:35 +0200

    Remove useless end tags.

--- about.php
+++ about.php
@@ -75,7 +75,7 @@
 
                     <div class="media">
                         <span class="sponsor-pic">
-                            <img src="img/sponsors/Cloudflare.png" alt=""/>
+                            <img src="img/sponsors/Cloudflare.png" alt="">
                         </span>
                         <div class="media-body">
                             <h4 class="media-heading">Cloudflare</h4>
@@ -84,7 +84,7 @@
                     </div>
 
                     <div class="media">
-                        <span class="sponsor-pic"><img src="img/sponsors/cedexis.png" alt=""/></span>
+                        <span class="sponsor-pic"><img src="img/sponsors/cedexis.png" alt=""></span>
                         <div class="media-body">
                             <h4 class="media-heading">Cedexis</h4>
                             <p>Cedexis allows us to do load balancing between an unlimited amount of CDN providers and failover in case one provider goes down.</p>
@@ -92,7 +92,7 @@
                     </div>
 
                     <div class="media">
-                        <span class="sponsor-pic"><img src="img/sponsors/maxcdn.png" alt=""/></span>
+                        <span class="sponsor-pic"><img src="img/sponsors/maxcdn.png" alt=""></span>
                         <div class="media-body">
                             <h4 class="media-heading">MaxCDN</h4>
                             <p>MaxCDN is the second main CDN provider we use. They offer us worldwide coverage and the best possible speed for all users around the globe.</p>
@@ -100,7 +100,7 @@
                     </div>
 
                     <div class="media">
-                        <span class="sponsor-pic"><img src="img/sponsors/builtwith.png" alt=""/></span>
+                        <span class="sponsor-pic"><img src="img/sponsors/builtwith.png" alt=""></span>
                         <div class="media-body">
                             <h4 class="media-heading">Builtwith</h4>
                             <p>Builtwith.com generously provides us with reports that help us track the usage of jsDelivr.</p>
@@ -108,7 +108,7 @@
                     </div>
 
                     <div class="media">
-                        <span class="sponsor-pic"><img src="img/sponsors/leapswitch.png" alt=""/></span>
+                        <span class="sponsor-pic"><img src="img/sponsors/leapswitch.png" alt=""></span>
                         <div class="media-body">
                             <h4 class="media-heading">Leapswitch</h4>
                             <p><a href="http://leapswitch.com">leapswitch</a> is sponsoring us with custom POP locations in Portugal and Ukraine.</p>
@@ -116,7 +116,7 @@
                     </div>
 
                     <div class="media">
-                        <span class="sponsor-pic"><img src="img/sponsors/prometeus.png" alt=""/></span>
+                        <span class="sponsor-pic"><img src="img/sponsors/prometeus.png" alt=""></span>
                         <div class="media-body">
                             <h4 class="media-heading">Prometeus</h4>
                             <p><a href="http://prometeus.net">Prometeus</a> is sponsoring us with custom POP location in Italy.</p>
@@ -124,7 +124,7 @@
                     </div>
 
                     <div class="media">
-                        <span class="sponsor-pic"><img src="img/sponsors/secureax.png" alt=""/></span>
+                        <span class="sponsor-pic"><img src="img/sponsors/secureax.png" alt=""></span>
                         <div class="media-body">
                             <h4 class="media-heading">SecureAX</h4>
                             <p><a href="http://www.secureax.com/">SecureAX</a> is sponsoring us with custom POP location in Singapore.</p>
@@ -132,7 +132,7 @@
                     </div>
 
                     <div class="media">
-                        <span class="sponsor-pic"><img src="img/sponsors/jetdino.png" alt=""/></span>
+                        <span class="sponsor-pic"><img src="img/sponsors/jetdino.png" alt=""></span>
                         <div class="media-body">
                             <h4 class="media-heading">Jetdino</h4>
                             <p><a href="https://jetdino.com">Jetdino</a> is sponsoring us with custom POP location in Indonesia.</p>
